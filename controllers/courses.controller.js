126b3847f2bff3175c1078c85d5215c281e14691
mahmoud142 <mahmoud.abdellah014@gmail.com>
2025-04-26 03:26:14 +0300

    new version with database

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,44 +1,57 @@
-let { courses } = require('../data/courses');
-const {validationResult } = require('express-validator');
 
-const getAllCourses = (req, res) => {
-    res.json(courses);
-}
+const { validationResult } = require('express-validator');
+const Course = require('../models/course.model');
 
-const getCourse = (req, res) => {
-    const courseId = +req.params.courseId;
-    const founded_course = courses.find(course => course.id == courseId);
-    if (!founded_course) {
-        return res.status(404).json({ msg: "Course not found" });
+
+const getAllCourses = async (req, res) => {
+    try {
+        const courses = await Course.find();
+        res.json(courses);
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error" });
     }
-    res.status(200).json(founded_course);
 }
 
-const addCourse =  (req, res) => {
+const getCourse = async (req, res) => {
+    try {   
+        const course = await Course.findById(req.params.courseId);
+        if (!course) {
+            return res.status(404).json({ msg: "Course not found" });
+        }
+        res.status(200).json(course);
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error" });
+    }
+}
+
+const addCourse = async (req, res) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
-        }
-        const course = { id: courses.length + 1, ...req.body };
-        courses.push(course);
-        res.status(201).json(course);
-}
-
-const updateCourse = (req, res) => {
-    const courseId = +req.params.courseId;
-    let course = courses.find(course => course.id === courseId);
-    if (!course) {
-        return res.status(404).json({ msg: "course not found" });
     }
-    console.log("passed");
-    course = { ...course, ...req.body };
-    res.status(200).json(course);
+    const newCourse = new Course(req.body);
+    await newCourse.save();
+    res.status(201).json(newCourse);
 }
 
-const deleteCourse = (req, res) => {
-    const courseId = +req.params.courseId;
-    courses = courses.filter(course => course.id != courseId)
-    res.status(200).json({ success: true });
+const updateCourse = async (req, res) => {
+    const courseId = req.params.courseId;
+    try {
+        const updatedCourse = await Course.findByIdAndUpdate(courseId, {$set: req.body});
+        res.status(200).json(updatedCourse);
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error" });
+    }
+}
+
+const deleteCourse = async (req, res) => {
+    try {
+        const courseId = req.params.courseId;
+        const deletedCourse = await Course.findByIdAndDelete(courseId);
+        res.status(200).json(deletedCourse);
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error" });
+    }
 }
 
 
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 app.use(express.json());
+
+const uri = process.env.URI;
+const mongoose = require('mongoose');
+mongoose.connect(process.env.URI).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+});
+
 const coursesRouter = require('./routes/courses.route')
 app.use('/api/courses', coursesRouter)
 
