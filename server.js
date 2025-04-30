125ab996f15e7e41ac05a1e2dac82e8c98b10d02
mahmoud142 <mahmoud.abdellah014@gmail.com>
2025-04-30 12:11:31 +0300

    Handle Wrong Routes

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.use(express.json());
-
-const uri = process.env.URI;
 const mongoose = require('mongoose');
-
 const httpStatusText = require('./utils/httpStatusText');
 
 mongoose.connect(process.env.URI).then(() => {
@@ -19,9 +16,9 @@ mongoose.connect(process.env.URI).then(() => {
 const coursesRouter = require('./routes/courses.route')
 app.use('/api/courses', coursesRouter)
 
-// Handle 404 for undefined routes 
+// Handle 404 for undefined routes
 app.use((req, res) => {
-    res.status(404).json({ status: httpStatusText.ERROR , message: "page not found"});
+    res.status(404).json({ status: httpStatusText.ERROR ,message: "Route not found"});
 });
 
 app.listen(PORT, () => {
