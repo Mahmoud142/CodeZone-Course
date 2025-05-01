4b3298b70c7f93bc62d1361aa5a9df5a3510b8c1
mahmoud142 <mahmoud.abdellah014@gmail.com>
2025-05-01 17:22:06 +0300

    asyncWrapper file

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,51 +1,49 @@
-
 const { validationResult } = require('express-validator');
 const Course = require('../models/course.model');
 
 const httpStatusText = require('../utils/httpStatusText');
-
+const asyncWrapper = require('../middlewares/asyncWrapper');
 const getAllCourses = async (req, res) => {
     try {
         // pagination
         const query = req.query;
-        const limit = query.limit || 10;    
+        const limit = query.limit || 10;
         const page = query.page || 1;
         const skip = (page - 1) * limit;
 
         const courses = await Course.find({}, { "__v": false }).limit(limit).skip(skip);
-        res.json({status: httpStatusText.SUCCESS, data: {courses}});
+        res.json({ status: httpStatusText.SUCCESS, data: { courses } });
     } catch (error) {
         res.status(500).json({ status: httpStatusText.ERROR, data: null, message: error.message });
     }
 }
 
-const getCourse = async (req, res) => {
-    try {   
-        const course = await Course.findById(req.params.id);
-        if (!course) {
-            console.log("Course not found");
-            return res.status(404).json({ status: httpStatusText.FAIL, data: {course : null} });
+const getCourse = asyncWrapper(
+    async (req, res,next) => {
+        try {
+            const course = await Course.findById(req.params.id);
+            return res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
+        } catch (error) {
+            error.message = "Course not found";
+            error.statusCode = 404;
+            next(error);
         }
-        res.status(200).json({ status: httpStatusText.SUCCESS, data: {course}});
-    } catch (error) {
-        res.status(500).json({ status: httpStatusText.ERROR, data: null, message: error.message ,code :400});
-    }
-}
+    })
 
 const addCourse = async (req, res) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ status: httpStatusText.FAIL, data: {errors: errors.array()} });
+        return res.status(400).json({ status: httpStatusText.FAIL, data: { errors: errors.array() } });
     }
     const newCourse = new Course(req.body);
     await newCourse.save();
-    res.status(201).json({ status: httpStatusText.SUCCESS, data: {course : newCourse} });
+    res.status(201).json({ status: httpStatusText.SUCCESS, data: { course: newCourse } });
 }
 
 const updateCourse = async (req, res) => {
     const courseId = req.params.id;
     try {
-        const updatedCourse = await Course.updateOne({_id: courseId}, {$set: req.body});
+        const updatedCourse = await Course.updateOne({ _id: courseId }, { $set: req.body });
         res.status(200).json({ status: httpStatusText.SUCCESS, data: { course: updatedCourse } });
     } catch (err) {
         res.status(500).json({ status: httpStatusText.ERROR, message: err.message });
@@ -56,11 +54,11 @@ const deleteCourse = async (req, res) => {
     try {
         const courseId = req.params.id;
         const deletedCourse = await Course.findByIdAndDelete(courseId);
-        res.status(200).json({ status: httpStatusText.SUCCESS, data: null});
+        res.status(200).json({ status: httpStatusText.SUCCESS, data: null });
     } catch (error) {
         res.status(500).json({ status: httpStatusText.ERROR, message: err.message });
     }
-} 
+}
 
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 require('dotenv').config();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
+
 const mongoose = require('mongoose');
 const httpStatusText = require('./utils/httpStatusText');
 
@@ -18,8 +22,12 @@ app.use('/api/courses', coursesRouter)
 
 // Handle 404 for undefined routes
 app.use((req, res) => {
-    res.status(404).json({ status: httpStatusText.ERROR ,message: "Route not found"});
+    return res.status(404).json({ status: httpStatusText.ERROR, message: "Route not found" });
 });
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json(err);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
